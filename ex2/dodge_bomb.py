05530b65c4c7e29afbc09bddf6c132154732a71b
c0a24131f8 <c0a24131f8@edu.teu.ac.jp>
2025-09-30 16:32:20 +0900

    gameover機能追加

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random
 import sys
 import pygame as pg
@@ -23,6 +24,38 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def gameover(screen: pg.Surface) -> None:
+    """
+    こうかとんに爆弾が衝突すると「Game Over」「こうかとんの画像」が5秒間表示される関数
+    黒い画面に「Game Over」「こうかとんの画像」をそれぞれ載せ、time.sleep()で5秒間表示させる
+    """
+    tannkei = pg.Surface((WIDTH, HEIGHT))
+    tannkei.set_alpha(255)
+    fonto = pg.font.Font(None, 100)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    tannkei.blit(txt, [350, 300])
+    kkgo_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    tannkei.blit(kkgo_img, [300, 300])
+    tannkei.blit(kkgo_img, [735, 300])
+    screen.blit(tannkei, [0, 0])
+    pg.display.update()
+    time.sleep(8)
+
+def init_bb_imgs() ->tuple[list[pg.Surface], list[int]]:
+    """
+    時間とともに爆弾を拡大、加速させる
+    大きさを変えた爆弾Surfaceのリストと加速度のリストを準備する
+    """
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+    return (bb_imgs, bb_accs)
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -30,6 +63,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
@@ -37,8 +71,12 @@ def main():
     bb_rct.centerx = random.randint(0,WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
+
+    bbimgs, bbaccs =  init_bb_imgs()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -46,6 +84,7 @@ def main():
         screen.blit(bg_img, [0, 0])
 
         if kk_rct.colliderect(bb_rct):
+            gameover(screen)
             return 
 
         key_lst = pg.key.get_pressed()
@@ -58,12 +97,18 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        bb_rct.move_ip(vx, vy)
+
+        avx = vx*bbaccs[min(tmr//500, 9)]
+        avy = vy*bbaccs[min(tmr//500, 9)]
+        bb_img = bbimgs[min(tmr//500, 9)]
+
+        bb_rct.move_ip(avx, avy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
+
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
